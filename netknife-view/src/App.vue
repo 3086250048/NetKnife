37b420b88ad6226095506c866a8803d2118eacbc
czz <czz@linux>
2023-03-09 11:48:18 +0800

    init_1

--- netknife-view/src/App.vue
+++ netknife-view/src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-      <button @click="get_test">点击获取测试</button>
+      <Meun></Meun>
   </div>
 </template>
 <script>
-import axios from 'axios'
+import meun from './components/meun.vue';
 export default{
-  name:'App',
-  methods:{
-    get_test(){
-      axios({
-        method:'GET',
-        url:'http://127.0.0.1:3000/get'
-      }).then(response=>{
-        console.log(response)
-      })
-    },
-  }
+    name:'App',
+    components:{
+      Meun:meun
+    }
 }
 </script>
